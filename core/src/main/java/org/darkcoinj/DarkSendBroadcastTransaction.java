98b535473a5a7193a13aca213f7a34d6e106b7e6
kalilinuxoo7 <hemant.singh.leu@gmail.com>
2018-02-11 12:54:43 +0530

    initial commmit

---
+++ core/src/main/java/org/darkcoinj/DarkSendBroadcastTransaction.java
@@ -0,0 +1,14 @@
+package org.darkcoinj;
+
+import org.pivxj.core.Transaction;
+import org.pivxj.core.TransactionInput;
+
+/**
+ * Created by Eric on 2/8/2015.
+ */
+public class DarkSendBroadcastTransaction {
+    Transaction tx;
+    TransactionInput vin;
+    byte [] vchSig;
+    long sigTime;
+}
